ba6c81319ed3bc4e4d4ad281778a59cec1539eaf
tuanalexeu <alekseytyan45@gmail.com>
2021-07-01 11:29:42 +0300

    Classes

---
+++ src/main/kotlin/com/alekseytyan/classes/ExtensionFunctions.kt
@@ -0,0 +1,7 @@
+package com.alekseytyan.classes
+
+fun Int.r(): RationalNumber = TODO()
+
+fun Pair<Int, Int>.r(): RationalNumber = TODO()
+
+data class RationalNumber(val numerator: Int, val denominator: Int)
