d869fb99c09002a2a0d6a112438b83ae91e77e27
tuanalexeu <alekseytyan45@gmail.com>
2021-07-06 13:14:44 +0300

    Refactoring

---
+++ src/main/kotlin/com/alekseytyan/convertions/util/MyDate.kt
@@ -0,0 +1,31 @@
+package com.alekseytyan.convertions.util
+
+import com.alekseytyan.convertions.DateRange
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val format = SimpleDateFormat("yyyy-MM-dd");
+        val date1 = format.parse("$year-$month-$dayOfMonth")
+        val date2 = format.parse("${other.year}-${other.month}-${other.dayOfMonth}")
+
+        return date1.compareTo(date2)
+    }
+}
+
+/*
+ * Returns the following date after the given one.
+ * For example, for Dec 31, 2019 the date Jan 1, 2020 is returned.
+ */
+fun MyDate.followingDate(): MyDate {
+    val c = Calendar.getInstance()
+    c.set(year, month, dayOfMonth)
+    val millisecondsInADay = 24 * 60 * 60 * 1000L
+    val timeInMillis = c.timeInMillis + millisecondsInADay
+    val result = Calendar.getInstance()
+    result.timeInMillis = timeInMillis
+    return MyDate(result.get(Calendar.YEAR), result.get(Calendar.MONTH), result.get(Calendar.DATE))
+}
+
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
