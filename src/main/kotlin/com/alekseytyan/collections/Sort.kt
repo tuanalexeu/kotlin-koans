f933d54763aa6946e0845fd335ce06de453d2d8f
tuanalexeu <alekseytyan45@gmail.com>
2021-07-10 16:34:06 +0300

    Sort (Collections)

---
+++ src/main/kotlin/com/alekseytyan/collections/Sort.kt
@@ -0,0 +1,5 @@
+package com.alekseytyan.collections
+
+// Return a list of customers, sorted in the descending by number of orders they have made
+fun Shop.getCustomersSortedByOrders(): List<Customer> =
+    TODO()
