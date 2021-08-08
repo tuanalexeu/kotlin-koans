78e7d6a8046d4e241fd0bc033bf478e8aeea7883
tuanalexeu <alekseytyan45@gmail.com>
2021-08-08 20:04:59 +0300

    Sequences; Get rid of duplicated method

--- src/main/kotlin/com/alekseytyan/collections/FlatMap.kt
+++ src/main/kotlin/com/alekseytyan/collections/FlatMap.kt
@@ -1,7 +1,7 @@
 package com.alekseytyan.collections
 
 // Return all products the given customer has ordered
-fun Customer.getOrderedProducts(): List<Product> = orders.flatMap(Order::products)
+//fun Customer.getOrderedProducts(): List<Product> = orders.flatMap(Order::products)
 
 // Return all products that were ordered by at least one customer
 fun Shop.getOrderedProducts(): Set<Product> = customers.flatMap(Customer::getOrderedProducts).toSet()
